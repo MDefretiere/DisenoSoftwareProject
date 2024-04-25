1e4e7f02e577b4609450ed7f9f7b5a7a52f326b4
Matthieu Defretiere <76651705+MDefretiere@users.noreply.github.com>
2024-04-25 12:01:25 +0200

    avancée

---
+++ DisenoSoftwareProject/src/main/java/com/mycompany/disenosoftwareproject/negocio/modelos/Llamada.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.disenosoftwareproject.negocio.modelos;
+
+/**
+ *
+ * @author defre
+ */
+public class Llamada {
+    private int id;
+    private String numeroTelefonoOrigen;
+    private Fecha fechaInicio;
+    private Hora horaInicio;
+    private Fecha fechaFin;
+    private Hora horaFin;
+    private String nombreComunicante;
+}
